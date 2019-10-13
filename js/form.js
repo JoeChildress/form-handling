6816e128757a70c65b77cc4734af8710228cca96
Joseph Childress <josephchildress@Josephs-MacBook-Pro.local>
2019-10-12 19:09:56 -0700

    validated passwords

--- js/form.js
+++ js/form.js
@@ -17,7 +17,7 @@ function validateFirstName(){
     //check if form is empty = return
     if (checkIfEmpty(firstName)) return;
     if (!checkIfOnlyLetters(firstName)) return;
-    return;
+    return true;
 }
 
 firstName.addEventListener('blur', validateFirstName)
@@ -26,22 +26,34 @@ function validateLastName(){
     //check if form is empty = return
     if (checkIfEmpty(lastName)) return;
     if (!checkIfOnlyLetters(lastName)) return;
-    return;
+    return true;
 }
 
 lastName.addEventListener('blur', validateLastName);
 
-//*** COMPLETE THIS ***/
+
 function validatePassword(){
     //check if form is empty = return
     if (checkIfEmpty(password)) return;
-    //if () return;
-    return;
+    if (!meetLength(password, 4, 20)) return;
+    if (!containsCharacters(password, "high")) return;
+    return true;
 }
 
-lastName.addEventListener('blur', validatePassword)
+password.addEventListener('blur', validatePassword);
 
+function validateConfirmPassword(){
+    //check if form is empty = return
+    if (checkIfEmpty(confirmPassword)) return;
+    if (!meetLength(confirmPassword, 4, 20)) return;
+    if (!containsCharacters(confirmPassword, "high")) return;
+    if (!matchesCharacters(confirmPassword, password, "Passwords do not match.")) return;
+    return true;
+}
 
+confirmPassword.addEventListener('blur', validateConfirmPassword);
+
+//Utility 
 function checkIfEmpty(field){
     if (isEmpty(field.value.trim())){
         // set field invalid
@@ -77,6 +89,64 @@ function checkIfOnlyLetters(field){
         return true;
     } else {
         setInvalid(field,`${field.name} must contain only letters.`);
+        return false;
+    }
+}
+
+function meetLength(field, low, high){
+    if (field.value.length >= low && field.value.length <= high){
+        setValid(field);
+        return true;
+    } else {
+        setInvalid(field,`${field.name} needs to be between ${low} to ${high} charcters in length.`)
+        return false;
+    }
+}
+
+function containsCharacters(field, level){
+    
+    let regEx,
+        message;
+
+    switch (level) {
+        case "low" :
+            regEx = new RegExp("^(?=.*[a-z])(?=.*[A-Z])");
+            message = `${field.name} must contain at least 1 uppercase, 1 lowercase charcter.`;
+            return matchWithRegEx(field,regEx,message);
+            break;
+
+        case "medium" :
+            regEx = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])");
+            message = `${field.name} must contain at least 1 uppercase, 1 lowercase, 1 number charcter.`;
+            return matchWithRegEx(field,regEx,message);
+            break;
+
+        //High is default
+        default :
+            regEx = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])");
+            message = `${field.name} must contain at least 1 uppercase, 1 lowercase, 1 number and 1 special charcter.`;
+            return matchWithRegEx(field,regEx,message);
+    }
+
+    
+}
+
+function matchWithRegEx(field,regEx,message){
+    if (field.value.match(regEx)){
+        setValid(field);
+        return true;
+    } else {
+        setInvalid(field,message);
+        return false;
+    }
+}
+
+function matchesCharacters(field,originalField, message){
+    if (field.value === originalField.value){
+        setValid(field);
+        return true;
+    } else {
+        setInvalid(field,message);
         return false;
     }
 }
